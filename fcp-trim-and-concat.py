6f10d34e9f0928c77ac65b85f6497534b293bf1f
Arthur <arthur.m.wilton@gmail.com>
2022-07-28 04:22:42 -0400

    major work on build_ffmpeg_filter()

--- fcp-trim-and-concat.py
+++ fcp-trim-and-concat.py
@@ -1,10 +1,13 @@
 import subprocess
 import json
-import time
+from collections import namedtuple
 
 output_file = "out.mp4"
 
-timecode_list = [('00\:00\:08.400', '00\:37\:49.767'), ('00\:37\:56.733', '00\:45\:28.167')]
+TimecodePair = namedtuple("TimecodePair", "in_point out_point")
+Filter = namedtuple("Filter", "arguments label")
+
+timecode_list = [TimecodePair('00\:00\:08.400', '00\:37\:49.767'), TimecodePair('00\:37\:56.733', '00\:45\:28.167')]
 
 filter_complex_command = """
                         [0:v]trim=start='00\:00\:08.400':end='00\:01\:49.767', setpts=PTS-STARTPTS, format=yuv420p[0v];
@@ -18,51 +21,56 @@ filter_complex_command = """
                         [0v][0audio_merged][1v][1audio_merged]concat=n=2:v=1:a=1[outv][outa]
                         """
 
-def iteration_test(timecode_list, stream_info, merge_audio=True):
+def build_ffmpeg_filter(timecode_list, stream_info, merge_audio=True):
 
-    for clip_number, timecode_pair in enumerate(timecode_list):
+    filter_commands_list = []
+    concat_filter_string = ""
+
+    for clip_number, timecode in enumerate(timecode_list):
         current_stream_index = 0
-        audio_stream_list = []
-        in_point = timecode_pair[0]
-        out_point = timecode_pair[1]
+        audio_merge_list = []
 
         while current_stream_index < len(stream_info):
             codec_type = stream_info[current_stream_index]['codec_type']
-            trim_string = build_trim_string(current_stream_index, in_point, out_point, clip_number, codec_type)
-            print(trim_string)
-            if codec_type == 'audio':
-                audio_stream_list.append(f"[clip_{clip_number}_{codec_type}_stream_{current_stream_index}]")
+            trim_string = build_trim_string(current_stream_index, timecode.in_point, timecode.out_point, codec_type)
+            label_string = build_label_string(clip_number, codec_type, current_stream_index)
+            filter_command = Filter(trim_string, label_string)
+            filter_commands_list.append(filter_command)
             
+            if (codec_type == 'audio') and (merge_audio):
+                audio_merge_list.append(filter_command.label)
+            elif (codec_type == 'audio') or (codec_type == 'video'):
+                concat_filter_string += filter_command.label
+
             current_stream_index += 1
 
-        if len(audio_stream_list) > 1:
-            audio_merge_string = build_audio_merge_string(audio_stream_list, clip_number)
-            print(audio_merge_string)
+        if merge_audio:
+            audio_merge_filter = build_audio_merge_filter(audio_merge_list, clip_number)
+            filter_commands_list.append(''.join(audio_merge_filter))
+            concat_filter_string += audio_merge_filter.label
 
+    concat_filter_string += f"concat=n={len(timecode_list)}:v=1:a=1[outv][outa]"
+    filter_commands_list.append(concat_filter_string)
 
-def build_trim_string(stream_index, in_point, out_point, clip_number, codec_type):
+    return filter_commands_list
+
+def build_trim_string(stream_index, in_point, out_point, codec_type):
     a_check = 'a' if codec_type == 'audio' else ''
     format_check = ',format=yuv420p' if codec_type == 'video' else ''
     trim_string = f"""
     [0:{stream_index}]{a_check}trim=start='{in_point}':end='{out_point},
     {a_check}setpts=PTS-STARTPTS
-    {format_check}[clip_{clip_number}_{codec_type}_stream_{stream_index}]
+    {format_check}
     """ 
     return trim_string
 
-def build_audio_merge_string(audio_stream_list, clip_number):
-    audio_merge_string = ""
+def build_label_string(clip_number, codec_type, stream_index):
+    return f"[clip_{clip_number}_{codec_type}_stream_{stream_index}]"
 
-    for audio_stream in audio_stream_list:
-        audio_merge_string += audio_stream
-    
-    audio_merge_string += f"amerge=inputs={len(audio_stream_list)} [{clip_number}_audio_merged]"
-
-    return audio_merge_string
-
-
-def build_ffmpeg_filter(stream_info, timecode_list):
-    ffmpeg_filter_string = ""
+def build_audio_merge_filter(audio_merge_list, clip_number):
+    arguments = f'{"".join(audio_merge_list)}amerge=inputs={len(audio_merge_list)}'
+    label = f'[clip_{clip_number}_audio_merged]'
+    return Filter(arguments, label)
 
 def get_stream_info_json(input_file):
     stream_info_string = subprocess.run(
@@ -84,9 +92,7 @@ def run_ffmpeg(input_file):
 def main():
     input_file = input("Enter file path: ")
     stream_info_dict = get_stream_info_json(input_file)
-    iteration_test(timecode_list, stream_info_dict['streams'])
-    # build_ffmpeg_filter(stream_info_dict, timecode_list)
-    # print(stream_info_dict)
+    build_ffmpeg_filter(timecode_list, stream_info_dict['streams'])
     # run_ffmpeg(input_file)
 
 if __name__ == "__main__":
